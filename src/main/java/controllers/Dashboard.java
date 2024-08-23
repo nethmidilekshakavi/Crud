14c97ff802a183359cbbafac44c7e824b8c1c0f8
nethmidilekshakavi <nethmidileksha6@gmail.com>
2024-08-23 09:34:08 +0530

    create login page

---
+++ src/main/java/controllers/Dashboard.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Dashboard {
+
+    @FXML
+    void btnStudent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnTeacher(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/controllers/Login.java
+++ src/main/java/controllers/Login.java
@@ -1,9 +1,47 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Login {
-    public void LoginToTheSystem(ActionEvent actionEvent) {
+
+    @FXML
+    private TextField PW;
+
+    @FXML
+    private TextField UN;
+
+    @FXML
+    private Button login;
+
+
+    @FXML
+    void LoginToTheSystem(ActionEvent event) throws IOException {
+
+        String username = "admin";
+        String password = "1234";
+
+        if (UN.getText().equals(username) && PW.getText().equals(password)) {
+
+            Parent parent = FXMLLoader.load(getClass().getResource("/controllers/view/dashboard.fxml"));
+            Scene scene = new Scene(parent);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("DashBoard");
+            stage.show();
+
+            Stage loginstage = (Stage) UN.getScene().getWindow();
+            loginstage.close();
+        }
 
     }
+
 }
